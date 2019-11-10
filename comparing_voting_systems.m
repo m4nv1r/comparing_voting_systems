9fd520f432378fecc83b3be53b220371712d755e
m4nv1r <45148145+m4nv1r@users.noreply.github.com>
2019-11-10 01:28:52 -0500

    Add example of how the function file can be used

---
+++ comparing_voting_systems.m
@@ -0,0 +1,10 @@
+% comparing voting systems
+
+% Initialization
+number_of_voters = 10;
+number_of_ridings = 3;
+number_of_political_parties = 3;
+number_of_simulations = 1;
+
+% misrepresentation ratio of each voting system
+res = voting_simulations(number_of_voters,number_of_ridings,number_of_political_parties,3,number_of_simulations)
